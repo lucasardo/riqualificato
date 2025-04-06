e980eeb12771f5a753cc97c615b0f4f7bfdc1f43
Luca Sardo <77811564+lucasardo@users.noreply.github.com>
2025-04-06 11:20:00 +0200

    Update home.py

--- app_pages/home.py
+++ app_pages/home.py
@@ -26,7 +26,7 @@ with col1:
     st.markdown(
         """
         <div style="text-align: center; margin-top: 20px;">
-            <a href="http://localhost:8502/barriera_da_vivere" class="red-button" style="display: inline-block; padding: 20px 20px; text-decoration: none; color: white;">Barriera da Vivere</a>
+            <a href="https://riqualificato-green-digital-futures.streamlit.app/barriera_da_vivere" class="red-button" style="display: inline-block; padding: 20px 20px; text-decoration: none; color: white;">Barriera da Vivere</a>
         </div>
         """,
         unsafe_allow_html=True,
@@ -37,7 +37,7 @@ with col2:
     st.markdown(
         """
         <div style="text-align: center; margin-top: 20px;">
-            <a href="http://localhost:8502/barriera_da_migliroare" class="red-button" style="display: inline-block; padding: 20px 20px; text-decoration: none; color: white;">Barriera da Migliorare</a>
+            <a href="https://riqualificato-green-digital-futures.streamlit.app/barriera_da_migliorare" class="red-button" style="display: inline-block; padding: 20px 20px; text-decoration: none; color: white;">Barriera da Migliorare</a>
         </div>
         """,
         unsafe_allow_html=True,
